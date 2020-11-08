3a7ad99549c2d429666c7ee1fcdc9bb88df029f7
Ahmed <github@tafakkur.live>
2020-11-09 03:04:50 +0530

    used foreach in deselect choices

--- JavaScript Files/Deselect Choices.js
+++ JavaScript Files/Deselect Choices.js
@@ -9,14 +9,14 @@
 
 // Question JS
 
-Qualtrics.SurveyEngine.addOnload(function() {
-	var questionId = this.questionId;
-	
-	jQuery('#Deselect1').click(function() {
-		for (var i = 1; i < 4; i++) {
-             //You'll need to change 4 to the number of choices you have. Sometimes for me Qualtrics goes crazy with its choice numbering, so i keep it at a high number like 500.
-			Qualtrics.SurveyEngine.registry[questionId].setChoiceValue(i, false);
-		}
-	});
+Qualtrics.SurveyEngine.addOnload(function () {
+	var questionId = this.questionId; //Get the Question Id
+	// Get the choices. This is needed as sometimes Qualtrics just goes crazy with choice numbers
+	nChoices = Qualtrics.SurveyEngine.registry[questionId].getChoices();
 
+	jQuery("#Deselect1").click(function () {
+		nChoices.forEach((item) => {
+			Qualtrics.SurveyEngine.registry[questionId].setChoiceValue(item,false);
+			});
+	});
 });
