0319192bb372c9c016fc4fca1918415091114325
Ahmed <github@tafakkur.live>
2021-01-14 06:34:36 +0530

    changed confirmation message

--- JavaScript Files/custom message request response.js
+++ JavaScript Files/custom message request response.js
@@ -1,6 +1,6 @@
 Qualtrics.SurveyEngine.addOnReady(function(){
     const qid = this.questionId;
-    tot_choices = Qualtrics.SurveyEngine.registry[qid].getChoices().length;
+    req_choices = 2;
     var sel_choices = 0;
     
     that = this;
@@ -12,10 +12,11 @@ Qualtrics.SurveyEngine.addOnReady(function(){
 
 
     function check_answers(){
-        if (sel_choices == tot_choices){that.clickNextButton();}
+        if (sel_choices == req_choices){that.clickNextButton();}
         else{
             //Selecting Okay will result in continuing
-            if (confirm("Custom message here") == true) {
+            cnf = confirm("You must select ONLY 2 options, you have selected " + sel_choices +".\n Do you still want to continue?" )
+            if (cnf) {
                 that.clickNextButton();
             } 
         }
